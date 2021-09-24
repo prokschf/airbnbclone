6583da104a4a9969fc1fa1999c86fb8889c17acb
prokschf <proksch.florian@googlemail.com>
2021-09-24 17:50:20 +0200

    fix typo

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <p></p>
+            <p>Footer</p>
         </div>
     )
-<div>
+}
     
-</div>export default Footer
+export default Footer
