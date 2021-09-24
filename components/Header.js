5baed861a8e6115eb65bd9aac9598fda416b71d4
prokschf <proksch.florian@googlemail.com>
2021-09-24 03:13:39 +0200

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+function Header() {
+    return (
+        <header>
+            <h1>I am the header</h1>
+
+            {/* left */}
+            <div>
+
+            </div>
+
+            {/* Middle */}
+
+            <div>
+
+            </div>
+
+            {/* Right */}
+            <div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header
