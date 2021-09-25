6e0ee72e943098c41485be2dbf054c3921f510e4
prokschf <proksch.florian@googlemail.com>
2021-09-25 08:43:23 +0200

    add progress and items

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,47 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-            <p>Footer</p>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14
+            bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Flo's Portfolio</p>
+                <p>Super</p>
+                <p>Website Design</p>
+                <p>Super Quality</p>
+                <p>Easy</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>About Us</p>
+                <p>Career Advice</p>
+                <p>Best Service</p>
+                <p>Great Offers</p>
+                <p>Inquire Now</p>
+            </div>
+
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help center</p>
+                <p>Customer Support</p>
+                <p>Lorem Ipsum</p>
+                <p>Vite Etalis</p>
+                <p>Supra Indentum</p>
+            </div>
+
+
+
         </div>
     )
 }
